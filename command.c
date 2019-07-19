c691ca4b68813ea19d887b272542d2d245f95356
Philip Simonson <psimonson1988@gmail.com>
2019-07-18 21:07:12 -0400

    Milestone got sectors reading for FAT12 root directory.

--- command.c
+++ command.c
@@ -25,25 +25,40 @@ asm("jmp main");
 #include "fat12.h"
 #include "disk.h"
 
+/* my nice header */
 #define INFOMSG "\x43\x4f\x44\x45\x44\x20\x42\x59\x20\x50\x48\x49\x4c\x49\x50\x00"
 
+/* external linkage; shell.c has this in it */
 extern int shell();
 
 /* boot sector */
 boot_t *_boot_sector;
 drive_params_t p;
-/*entry_t *_entry;
-*/
+/*entry_t *_entry;*/
+
 /* Entry point for my command shell.
  */
 void main()
 {
+/*	static entry_t *file; */
+
+	/* get first floppy drive */
 	if(get_drive_params(&p, 0))
 		goto end;
+	/* load boot sector into memory */
 	if((_boot_sector = load_boot(&p)) == 0)
 		goto end;
+
+	/* load root directory and list files */
 /*	_entry = load_root(&p, _boot_sector);
-	printf("%d\r\n", _entry[0].filename[0]);
+	for(file=_entry; file->filename[0] != 0; ++file) {
+		if(file->filename[0] == 0xe5) {
+			int i;
+			for(i=1; i<11; i+=2)
+				printf("%c", file->filename[i]);
+			printf("\r\n");
+		}
+	}
 */
 	/* start of actual command mode */
 	printf("Press any key to continue...");
